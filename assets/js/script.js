7b31e8509d9e06ea43d63e814c7f89106ab65f20
Mahim Ibrahimi <mahimibrahimi@gmail.com>
2025-08-24 10:35:37 +0330

    Implement dark/light themes
    - Add dark theme variables
    - Implement theme icon functionality
    - Show correct icon on each theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 // Show Menu
-const showMenu = () => {
-    console.log("Showing Menu");
-    const toggle = document.querySelector('.menu-icon');
-    const menu = document.querySelector('.nav-menu');
+console.log("Showing Menu");
+const toggle = document.querySelector('.menu-icon');
+const menu = document.querySelector('.nav-menu');
 
-    toggle.addEventListener('click', () => {
-        menu.classList.toggle('show-menu');
-    })
-}
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+})
 
-showMenu();
+
+// Dark / Light Theme
+const themeBtn = document.querySelector('#theme-icon');
+console.log(themeBtn);
+
+themeBtn.addEventListener('click', () => {
+    const currentTheme = document.body.className;
+
+    if (currentTheme === 'dark') {
+        themeBtn.classList.replace("bx-sun", "bx-moon");
+    } else {
+        themeBtn.classList.replace("bx-moon", "bx-sun");
+    }
+    document.body.classList.toggle('dark');
+})
 
